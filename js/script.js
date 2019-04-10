126549ae7e09c745082a8ae6b38d9ad4aac98339
Kat Podmogilnaya <kat.tigra@gmail.com>
2019-04-10 09:28:18 +0300

    Домашнее задание второго дня интенсива

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const switcher = document.querySelector('#cbx'), 
     more = document.querySelector('.more'),
     modal = document.querySelector('.modal'),
-    videos = document.querySelectorAll('.videos_item');
+    videos = document.querySelectorAll('.videos__item');
 let player;
 
 function bindSlideToggle(trigger, boxBody, content, openClass) {
@@ -31,7 +31,7 @@ bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-acti
 function switchMode() {
     if (night === false) {
         night = true;
-        //document.body.style.backgroundColor = '#000';
+        
         document.body.classList.add('night');
         document.querySelectorAll('.hamburger > line').forEach(item => {
             item.style.stroke = '#fff';
@@ -99,6 +99,92 @@ more.addEventListener('click', () => {
         videosWrapper.appendChild(card);
         setTimeout(() => {
             card.classList.remove('videos__item-active');
-        }, 10)
+        }, 10);
+        bindNewModal(card);
+        if (night) {
+            card.style.color = '#fff';
+        } 
     }
+
+    sliceTitle('.videos__item-descr', 100);
 })
+
+function sliceTitle(selector, count) {
+    document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+
+        if (item.textContent.length < count) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count) + "...";
+            item.textContent = str;
+        }
+    });
+}
+
+sliceTitle('.videos__item-descr', 100);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    player.stopVideo();
+}
+
+function bindModal(cards) {
+    cards.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = item.getAttribute('data-url');
+            loadVideo(id);
+            openModal();
+        });
+    });
+}
+
+bindModal(videos);
+
+function bindNewModal(card) {
+    card.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = card.getAttribute('data-url');
+        loadVideo(id);
+        openModal();  
+    });  
+}
+
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')) {
+        closeModal();    
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+     if (e.keyCode === 27) { 
+         closeModal();
+     }
+});
+
+function createVideo() {
+
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+   
+    setTimeout(() => {
+        player = new YT.Player('frame', {
+            height: '100%',
+            width: '100%',
+            videoId: 'M7lc1UVf-VE',
+        });
+    }, 300);   
+}
+
+createVideo();
+
+function loadVideo(id) {
+    player.loadVideoById({'videoId': `${id}`});
+}
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,4 +1,9 @@
-   let button = {
-        'element': document.querySelector(trigger)
+const strTrim = item.textContent.trim();
+        item.textContent = strTrim;
 
-    };
+        if (item.textContent.length < count) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count+1) + "...";
+            item.textContent = str;
+        }
